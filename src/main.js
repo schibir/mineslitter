5410736e95ca178849a62a4eca7a01b97d5fcb64
Sergey Chibiryaev <zav.gamedev@gmail.com>
2017-11-11 13:11:31 +0300

    added main.js

---
+++ src/main.js
@@ -0,0 +1,15 @@
+
+function main() {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+
+    canvas.addEventListener("mouseup", (event) => {
+    }, false);
+
+    canvas.addEventListener("mousedown", (event) => {
+    }, false);
+
+    canvas.addEventListener("mousemove", (event) => {
+    }, false);
+}
+document.addEventListener("DOMContentLoaded", main);
