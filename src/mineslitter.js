2f61d85cf464c360a8dfd529b8a633037cf53e19
Sergey Chibiryaev <zav.gamedev@gmail.com>
2017-11-12 09:58:33 +0300

    generate fields

--- src/mineslitter.js
+++ src/mineslitter.js
@@ -1,4 +1,25 @@
 
+class Tile {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.value = 0;
+        this.mine = false;
+    }
+
+    draw(ctx, img, scale) {
+        const draw = (sx, sy) => {
+            ctx.drawImage(img, sx, sy, 16, 16,
+                (12 + 16 * this.x) * scale, (55 + 16 * this.y) * scale, 16 * scale, 16 * scale);
+        };
+
+        if (this.value === 0) draw(0, 0);
+        else if (this.value > 0 && this.value <= 8) {
+            draw(16 + 16 * ((this.value - 1) & 3), 16 * (this.value - 1 >> 2));
+        }
+    }
+}
+
 export default class Mineslitter {
     constructor(context) {
         this.context = context;
@@ -22,14 +43,61 @@ export default class Mineslitter {
                     this.height * 16 * this.scale + 67 * this.scale);
         /* eslint-enable indent */
 
-        this.startTime = Date.now();
+        this.startGame();
 
         this.draw();
+    }
+
+    startGame() {
+        this.generate();
+        this.startTime = Date.now();
+
+        this.timerId = setInterval(() => this.drawTime(), 100);
     }
 
     stopGame() {
         if (this.timerId) clearInterval(this.timerId);
         this.timerId = 0;
+    }
+
+    adjacentTiles(tile, callback) {
+        for (let x = tile.x - 1; x <= tile.x + 1; x++) {
+            for (let y = tile.y - 1; y <= tile.y + 1; y++) {
+                if (x >= 0 && x < this.width && y >= 0 && y < this.height) {
+                    callback(this.tiles[x + y * this.width]);
+                }
+            }
+        }
+    }
+
+    generate() {
+        const rand = (n) => Math.random() * n | 0;
+
+        // generate all mines
+        const mines = new Array(this.width * this.height);
+        for (let i = 0; i < this.mines; i++) {
+            const ind = rand(this.width * this.height);
+            if (mines[ind]) i--;
+            else mines[ind] = true;
+        }
+
+        this.tiles = new Array(this.width * this.height);
+        for (let i = 0; i < this.tiles.length; i++) {
+            const x = i % this.width | 0;
+            const y = i / this.width | 0;
+            this.tiles[i] = new Tile(x, y);
+        }
+
+        // calc value for mineless tiles
+        this.tiles.forEach((tile) => {
+            let count = 0;
+            this.adjacentTiles(tile, (next) => {
+                const ind = next.x + next.y * this.width;
+                if (mines[ind]) count++;
+            });
+            const ind = tile.x + tile.y * this.width;
+            this.tiles[ind].value = count;
+        });
     }
 
     /* Draw */
@@ -68,9 +136,9 @@ export default class Mineslitter {
             dx * this.scale, dy * this.scale, dwidth * this.scale, dheight * this.scale);
     }
 
-    // drawTile(tile) {
-    //     tile.draw(this.context, this.img_tiles, this.scale);
-    // }
+    drawTile(tile) {
+        tile.draw(this.context, this.img_tiles, this.scale);
+    }
 
     draw() {
         this.drawRect(0, 0, 12, 55, 0, 0, 12, 55);
@@ -84,7 +152,7 @@ export default class Mineslitter {
         this.drawRect(52, 0, 41, 25, 16, 16, 41, 25);
         this.drawRect(52, 0, 41, 25, 12 + this.width * 16 - 4 - 41, 16, 41, 25);
 
-        // this.tiles.forEach((tile) => this.drawTile(tile, false));
+        this.tiles.forEach((tile) => this.drawTile(tile, false));
 
         this.drawMinesLeft();
         this.drawTime();
